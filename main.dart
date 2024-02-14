ab36dea65e225dd6fd417ad41c5d1903403233a3
clo901 <93842458+clo901@users.noreply.github.com>
2024-02-13 20:01:52 -0800

    Update main.dart
    Import and Class MyApp

--- main.dart
+++ main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Robot Delivery',
+      home: AddressSelectionPage(),
+    );
+  }
+}
